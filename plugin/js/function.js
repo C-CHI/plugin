aceda7b359c2ce3d337c45463137857a42744af6
池清澄 <chichongjing@ikeseichou-no-MacBook-Pro.local>
2018-07-31 11:14:42 +0900

    アコーディオンmore表示の処理追加

--- plugin/js/function.js
+++ plugin/js/function.js
@@ -2,7 +2,8 @@
 var LEIHAUOLI_PLUGIN = LEIHAUOLI_PLUGIN || {};
 
 LEIHAUOLI_PLUGIN_ACCORDION = function($wrapper,timer){
-    this.$menu = $wrapper;
+    this.$wrapper = $wrapper;
+    this.hideValue = 0;
     this.changeTime = timer;
     this.init();
 };
@@ -15,11 +16,11 @@ LEIHAUOLI_PLUGIN_ACCORDION.prototype = {
     },
 
     setParamerters : function(){
-        this.$trigger = this.$menu.children('dt');  //メニュー
-        this.$contents = this.$menu.children('dd'); //コンテンツ
-        this.contentsHeight = this.$contents.outerHeight();
-        this.contentsPaddingTop = this.$contents.css('paddingTop');
-        this.contentsPaddingBottom = this.$contents.css('paddingBottom');
+        this.$trigger = this.$wrapper.children('dt');  //メニュー
+        this.$contents = this.$wrapper.children('dd'); //コンテンツ
+        this.contentsHeight = this.$contents.outerHeight(); //コンテンツ高さ
+        this.contentsPaddingTop = this.$contents.css('paddingTop'); //コンテンツ上余白
+        this.contentsPaddingBottom = this.$contents.css('paddingBottom'); //コンテンツ下余白
     },
 
     bindEvent : function(){
@@ -35,9 +36,9 @@ LEIHAUOLI_PLUGIN_ACCORDION.prototype = {
         var flg = $target.next().is(':hidden');
 
         this.$contents.filter(':visible').animate({
-            'height':'0',
-            'paddingTop':'0',
-            'paddingBottom':'0'
+            'height':this.hideValue,
+            'paddingTop':this.hideValue,
+            'paddingBottom':this.hideValue
         },this.changeTime,function(){
             $(this).css('display','none');
         });
@@ -46,9 +47,9 @@ LEIHAUOLI_PLUGIN_ACCORDION.prototype = {
             
             $target.next().css({
                 'display':'block',
-                'height':'0',
-                'paddingTop':'0',
-                'paddingBottom':'0'
+                'height':this.hideValue,
+                'paddingTop':this.hideValue,
+                'paddingBottom':this.hideValue
             });
 
             $target.next().animate({
@@ -62,6 +63,73 @@ LEIHAUOLI_PLUGIN_ACCORDION.prototype = {
 
 }
 
-$(window).on('load resize',function(){
+LEIHAUOLI_PLUGIN_ACCORDION_MORE_DISPLAY = function($wrapper,$trigger,animationSpeed,firstDisplay,nextDisplay){
+    this.$wrapper = $wrapper;
+    this.$triggerBox = $trigger;
+    this.firstDisplay = firstDisplay;
+    this.nextDisplay = nextDisplay;
+    this.animationSpeed = animationSpeed;
+    this.init();
+};
+
+LEIHAUOLI_PLUGIN_ACCORDION_MORE_DISPLAY.prototype = {
+    
+    init : function(){
+        this.setParamerters();
+        this.bindEvent();
+        this.defaultContents();
+    },
+
+    setParamerters : function(){
+        this.$window = $(window);
+        this.$list = this.$wrapper.children('li');
+        this.$trigger = this.$triggerBox.children('a');
+    },
+
+    bindEvent : function(){
+        var myself = this;
+
+        this.$trigger.on('click',function(e){
+            e.preventDefault();
+            myself.changeContents();
+        });
+
+        this.$window.on('resize',function(){
+            myself.contentsResize();
+        });
+    },
+
+    defaultContents : function(){
+        this.contentsHeight = 0;
+        this.index = 0;
+
+        for(var i = 0; i <= this.firstDisplay-1; i++){
+            this.contentsHeight += this.$list.eq(i).outerHeight();
+            this.index++;
+        };
+
+        this.$wrapper.height(this.contentsHeight);
+    },
+
+    changeContents : function(){
+        if(this.index >= 10) return;
+
+        for(var i = 0; i <= this.nextDisplay-1; i++){
+            this.contentsHeight += this.$list.eq(this.index).outerHeight();
+            this.index++;
+        }
+        
+        this.$wrapper.animate({'height':this.contentsHeight},this.animationSpeed);
+    },
+
+    contentsResize : function(){
+        this.defaultContents();
+    }
+
+}
+
+$(window).on('load',function(){
     new LEIHAUOLI_PLUGIN_ACCORDION($('.accordion'),600);
+    new LEIHAUOLI_PLUGIN_ACCORDION($('.accordion2'),600);
+    new LEIHAUOLI_PLUGIN_ACCORDION_MORE_DISPLAY($('.accordion-wrapper'),$('.trigger'),600,3,2);
 });
